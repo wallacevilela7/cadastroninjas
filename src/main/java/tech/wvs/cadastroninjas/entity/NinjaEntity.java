f72e23321b466d7ac53a039aac7a7b9c02c4d9e1
wallacevilela7 <wallace.vilela9@gmail.com>
2025-02-26 15:49:58 -0300

    Definindo entidade Ninja e mapeando para o banco de dados com JPA

---
+++ src/main/java/tech/wvs/cadastroninjas/entity/NinjaEntity.java
@@ -0,0 +1,57 @@
+package tech.wvs.cadastroninjas.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "table_ninjas")
+public class NinjaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "age")
+    private Integer age;
+
+
+    public NinjaEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setIdade(Integer age) {
+        this.age = this.age;
+    }
+}
