f6707ea17f69e9666618efc7846e2e04057fcfe7
wallacevilela7 <wallace.vilela9@gmail.com>
2025-02-26 14:53:20 -0300

    Create app

---
+++ src/main/java/tech/wvs/cadastroninjas/CadastroninjasApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.cadastroninjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroninjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroninjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/wvs/cadastroninjas/CadastroninjasApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.cadastroninjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroninjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
